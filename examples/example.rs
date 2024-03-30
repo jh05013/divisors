11481abc7714b16e4c447dfece8c230ec4334f9c
jh05013 <jh05013@naver.com>
2024-03-30 21:50:55 +0900

    prototype

--- examples/example.rs
+++ examples/example.rs
@@ -1,10 +1,4 @@
 use divisors_fixed;
 use std::time::Instant;
 
-fn main() {
-    let n: u128 = 934832147123321;
-    println!("finding divisors of {}", n);
-    let start_time = Instant::now();
-    let v = divisors_fixed::get_divisors(n);
-    println!("time = {:?}, divisors = {:?}", start_time.elapsed(), v);
-}
+fn main() {}
--- src/lib.rs
+++ src/lib.rs
@@ -11,15 +11,111 @@
 
 use num_traits::{NumCast, PrimInt, Unsigned};
 use std::fmt::Display;
+use std::iter;
 
-pub trait Num: NumCast + Unsigned + Copy + PrimInt + Display {}
+pub trait Divisors: Sized {
+    fn divisors(&self) -> Vec<Self>;
+}
 
-impl Num for u8 {}
-impl Num for u16 {}
-impl Num for u32 {}
-impl Num for u64 {}
-impl Num for u128 {}
-impl Num for usize {}
+impl Divisors for u32 {
+    fn divisors(&self) -> Vec<Self> {
+        fn repeat_division(mut n: u32, d: u32) -> (u32, usize) {
+            for rep in 0.. {
+                // this will be optimized by LLVM
+                let (new_n, rem) = (n / d, n % d);
+
+                if rem != 0 {
+                    return (n, rep);
+                }
+                n = new_n;
+            }
+            unreachable!();
+        }
+
+        if *self == 0 {
+            return vec![];
+        }
+
+        let mut v = vec![1];
+
+        let mut n = *self;
+        for d in std::iter::once(2).chain((3..).step_by(2)) {
+            if d * d > n {
+                break;
+            }
+            let (new_n, rep) = repeat_division(n, d);
+            n = new_n;
+
+            v.reserve(v.len() * (rep + 1));
+            for i in 0..v.len() * rep {
+                let vi = unsafe { v.get_unchecked(i) };
+                let new_div = vi * d;
+                v.push(new_div);
+            }
+        }
+        if n > 1 {
+            v.reserve(v.len() * 2);
+            for i in 0..v.len() {
+                let vi = unsafe { v.get_unchecked(i) };
+                let new_div = vi * n;
+                v.push(new_div);
+            }
+        }
+
+        v.sort_unstable();
+        v
+    }
+}
+
+impl Divisors for u64 {
+    fn divisors(&self) -> Vec<Self> {
+        fn repeat_division(mut n: u64, d: u64) -> (u64, usize) {
+            for rep in 0.. {
+                // this will be optimized by LLVM
+                let (new_n, rem) = (n / d, n % d);
+
+                if rem != 0 {
+                    return (n, rep);
+                }
+                n = new_n;
+            }
+            unreachable!();
+        }
+
+        if *self == 0 {
+            return vec![];
+        }
+
+        let mut v = vec![1];
+
+        let mut n = *self;
+        for d in std::iter::once(2).chain((3..).step_by(2)) {
+            if d * d > n {
+                break;
+            }
+            let (new_n, rep) = repeat_division(n, d);
+            n = new_n;
+
+            v.reserve(v.len() * (rep + 1));
+            for i in 0..v.len() * rep {
+                let vi = unsafe { v.get_unchecked(i) };
+                let new_div = vi * d;
+                v.push(new_div);
+            }
+        }
+        if n > 1 {
+            v.reserve(v.len() * 2);
+            for i in 0..v.len() {
+                let vi = unsafe { v.get_unchecked(i) };
+                let new_div = vi * n;
+                v.push(new_div);
+            }
+        }
+
+        v.sort_unstable();
+        v
+    }
+}
 
 /// Return a vector with all divisors ordered of n in range [1, n].
 ///
@@ -34,79 +130,6 @@ impl Num for usize {}
 /// let v = divisors_fixed::get_divisors(n);
 /// println!("time = {:?}, divisors = {:?}", start_time.elapsed(), v);
 /// ```
-pub fn get_divisors<T: Num>(n: T) -> Vec<T> {
-    if n == T::zero() {
-        return vec![];
-    }
-
-    let zero: T = T::zero();
-    let one: T = T::one();
-    let two: T = T::from(2).unwrap();
-    let mut _n = n;
-    let mut v: Vec<T> = Vec::new();
-
-    let mut count_divisors_2: usize = 0;
-    while _n & one == zero {
-        v.push(two << count_divisors_2);
-        count_divisors_2 += 1;
-        _n = _n >> 1;
-    }
-
-    let mut _x: T = T::from(3).unwrap();
-    let mut _n_sqrt: T = approximated_sqrt(_n);
-    while _x < _n_sqrt {
-        let mut _pow_x = _x;
-        let v_len = v.len();
-        let mut x_is_a_divisors = false;
-
-        let mut pow_x_is_a_divisors = _n % _x == zero;
-        while pow_x_is_a_divisors {
-            _n = _n.div(_x);
-            v.push(_pow_x);
-            push_new_divisors(&mut v, v_len, _pow_x);
-            pow_x_is_a_divisors = _n % _x == zero;
-            if pow_x_is_a_divisors {
-                _pow_x = _pow_x.mul(_x);
-            }
-            x_is_a_divisors = true;
-        }
-        _x = _x + two;
-        if x_is_a_divisors {
-            _n_sqrt = approximated_sqrt(_n);
-        }
-    }
-
-    if _n > one && _n != n {
-        let v_len = v.len();
-        v.push(_n);
-        push_new_divisors(&mut v, v_len, _n);
-    }
-
-    if v.is_empty() {
-        v.push(n);
-    }
-    if n != one {
-        v.push(one);
-    }
-    v.sort();
-
-    v
-}
-
-fn approximated_sqrt<T: Num>(n: T) -> T {
-    let zero: T = T::zero();
-    let one: T = T::one();
-    let mut num_bits = (std::mem::size_of::<T>() << 3) - 1;
-    while ((n >> num_bits) & one) == zero {
-        num_bits -= 1;
-    }
-
-    one << ((num_bits >> 1) + 1)
-}
-
-/// Iterate on v from 0 to v_len and for each element e: push (e * _x) in v.
-fn push_new_divisors<T: Num>(v: &mut Vec<T>, v_len: usize, _x: T) {
-    for i in 0..v_len {
-        v.push(_x.mul(unsafe { *v.get_unchecked(i) }));
-    }
+pub fn get_divisors(n: u32) -> Vec<u32> {
+    n.divisors()
 }
